c9ac1e44486f890e5d65065c26c70d79cc08290c
Andrii <andrejveremcuk712@gmail.com>
2025-06-07 23:49:33 +0300

    fix: types error

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,16 +1,15 @@
 //import { Box } from '~/components/Box';
-import {BuubleChart} from '~/components/bubble-chart';
-import Header from '~/components/Header';
-import {getFinnData} from '~/services/finance-data/fmp-api';
+import { BuubleChart } from '~/components/bubble-chart';
+import { getFinnData } from '~/services/finance-data/fmp-api';
 
 export default async function Home() {
+  const data = await getFinnData();
 
-const data = await  getFinnData();
+  console.log(data);
 
   return (
     <>
-		<BuubleChart stockDataList={data}/>
-
+      <BuubleChart stockDataList={data.slice(0, 101)} />
     </>
   );
 }
