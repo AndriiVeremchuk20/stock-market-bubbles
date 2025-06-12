3981efd3d5ad6735c46b72522ec95f19e6e3520a
Andrii <andrejveremcuk712@gmail.com>
2025-06-12 19:31:11 +0300

    style: update header

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,15 +1,31 @@
+'use client';
 //import { Box } from '~/components/Box';
 import { BuubleChart } from '~/components/bubble-chart';
-import { getFinnData } from '~/services/finance-data/fmp-api';
+import useSWR from 'swr';
+import { Stock } from '~/services/fmp-api';
+import { AppStore } from '~/store/app';
+import Header from '~/components/header';
 
-export default async function Home() {
-  const data = await getFinnData();
+const fetcher = (url: string) => fetch(url).then((r) => r.json());
+
+export default function Home() {
+  const { skip, limit } = AppStore((s) => s);
+  const { data, error, isLoading } = useSWR<{ data: Stock[] }>(
+    `http://localhost:3000/api/stock/screener?skip=${skip}&limit=${limit}`,
+    fetcher,
+    { refreshInterval: 5 * 60 * 1000 }
+  );
 
   console.log(data);
 
-  return (
-    <>
-      <BuubleChart stockDataList={data.slice(0, 101)} />
-    </>
+  if (isLoading) return <div>Wait</div>;
+
+  return ( 
+    <main className='h-screen w-full bg-primary text-secondary'>
+    <Header/>  
+	<div className='w-full h-full'>
+	 <BuubleChart stockDataList={data!.data} />
+	</div>
+    </main>
   );
 }
