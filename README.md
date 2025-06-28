ef2980231aacb9ca8b1604b1faf0976103d2c1a2
Andrii <andrejveremcuk712@gmail.com>
2025-06-28 22:50:13 +0300

    feat: convert home page to a server component

--- README.md
+++ README.md
@@ -1,8 +1,8 @@
 ##To do:
 
-1. -> cache middleware
+1. -> [DONE] cache middleware
 2. -> process errors on client
-3. -> make /app/page.tsx a server component
-4. -> create settins (bubble size by vield, display market cap or price or volume)
+3. -> [DONE] make /app/page.tsx a server component
+4. -> create settins (bubble size by vield, display market cap or price or volume, theme)
 5. -> also display data in table (tanstack table)
-6. -> check image before getting to client
+6. -> [DONE] check image before getting to client
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,44 +1,15 @@
-'use client';
-//import { Box } from '~/components/Box';
-import { BuubleChart } from '~/components/bubble-chart';
-import useSWR from 'swr';
-import { Stock } from '~/server/services/fmp-api';
-import { AppStore } from '~/store/app';
 import Header from '~/components/header';
 import Footer from '~/components/footer';
-import { Skeleton } from '~/components/skeleton';
+import StockChartWrapper from '~/components/stock-chart-wrapper';
 
-const fetcher = (url: string) => fetch(url).then((r) => r.json());
 
-export default function Home() {
-  const { skip, limit, sort } = AppStore((s) => s);
-
-  const queryParams = new URLSearchParams();
-  if (typeof skip === 'number') queryParams.append('skip', skip.toString());
-  if (typeof limit === 'number') queryParams.append('limit', limit.toString());
-  if (sort === 'losers') queryParams.append('betaLowerThan', '-1');
-  if (sort === 'gainers') queryParams.append('betaMoreThan', '3');
-
-  const { data, error, isLoading } = useSWR<{ data: Stock[] }>(
-    `/api/stock/screener?${queryParams.toString()}`,
-    fetcher,
-    { refreshInterval: 60 * 3600 }
-  );
+export default async function Home() {
 
   return (
     <main className='w-full bg-primary text-secondary'>
       <Header />
-      <div className='h-screen w-screen'>
-        {isLoading ? (
-          <Skeleton />
-        ) : (
-          <BuubleChart
-            stockDataList={data!.data}
-            key={`key:${skip}${limit}${sort}`}
-          />
-        )}
-      </div>
-      <Footer />
+      <StockChartWrapper/>
+	  <Footer />
     </main>
   );
 }
