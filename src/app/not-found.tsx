dac10519706f9fb2c775287265d7041555796ac8
Andrii <andrejveremcuk712@gmail.com>
2025-06-29 21:34:16 +0300

    feat: 404 page

---
+++ src/app/not-found.tsx
@@ -0,0 +1,13 @@
+
+export default function NotFoundPage(){
+
+	return <main className="h-screen bg-primary flex justify-center items-center">
+	<div className="flex flex-col border border-secondary text-secondary p-4 rounded-md">
+	<h2 className="text-4xl border-b text-center font-bold">404</h2>
+<span className="text-secondary pt-2">
+
+	This page could not be found.
+</span>
+	</div>	
+	</main>
+}
